561e4b6c8d206848e071875154cb038f20dae5d6
Krisztian Balla <kriszi.balla@gmail.com>
2017-03-17 13:40:43 +0000

    feat(babel): add babel with es2015 and stage-0 presets

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,6 +6,17 @@ const config = {
     filename: 'bundle.js',
     publicPath: '',
     path: path.join(__dirname, '..', 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader'
+        }]
+      }
+    ]
   }
 }
 
