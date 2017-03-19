1d5d3830ff56171ae660d894cdba7dfc4de3611c
Krisztian Balla <kriszi.balla@gmail.com>
2017-03-19 23:52:54 +0000

    feat(sass-loader): add sass-loader, refactor webpack.config.js and add ip to make dev server accessable on network

--- config/project.config.js
+++ config/project.config.js
@@ -1,6 +1,19 @@
+const path = require('path')
+const ip = require('ip')
+
+const basePath = path.join(__dirname, '..')
+
 const config = {
+  paths: {
+    base: basePath,
+    client: path.join(basePath, 'src'),
+    dist: path.join(basePath, 'dist'),
+    public: path.join(basePath, 'public'),
+    server: path.join(basePath, 'server'),
+    tests: path.join(basePath, 'tests')
+  },
   // Server Configuration
-  server_host :'localhost',
+  server_host : ip.address(),
   server_port : process.env.PORT || 3000
 }
 
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,77 +1,110 @@
-const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const debug = require('debug')('app:config:webpack')
 
 const project = require('./project.config')
 
-const DEVELOPMENT = process.env.NODE_ENV === 'development'
-const PRODUCTION = process.env.NODE_ENV === 'production'
+const __DEV__ = process.env.NODE_ENV === 'development'
+const __PROD__ = process.env.NODE_ENV === 'production'
+const __TEST__ = process.env.NODE_ENV === 'test'
 
-const entry = ['./src/index.js']
-
-if (DEVELOPMENT) {
-  entry.push('webpack/hot/dev-server')
-  entry.push(`webpack-dev-server/client?http://${project.server_host}:${project.server_port}`)
-}
-
-const plugins = PRODUCTION
-  ? [
-    new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin('style-[contenthash:10].css')
-  ]
-  : [ new webpack.HotModuleReplacementPlugin() ]
-
-plugins.push(
-  new HTMLWebpackPlugin({
-    template: 'src/index.html'
-  })
-)
-
-const cssIdentifier = PRODUCTION ? '[hash:base64:10]' : '[path][name]---[local]'
-
-const cssLoader = PRODUCTION
-  ? ExtractTextPlugin.extract({
-    use: 'css-loader?localIdentName=' + cssIdentifier
-  })
-  : [{ loader: 'style-loader' }, { loader: 'css-loader?localIdentName=' + cssIdentifier }]
-
+// Shared Configuration
 const config = {
-  entry: entry,
+  entry: ['./src/index.js'],
   output: {
     filename: 'bundle.[hash:12].min.js',
     publicPath: '',
-    path: path.join(__dirname, '..', 'dist')
+    path: project.paths.dist
   },
-  devtool: PRODUCTION ? false : 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader'
-        }]
-      },
-      {
-        test: /\.(woff|png|jpg|gif)$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'images/[hash:12].[ext]'
-          }
-        }]
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: cssLoader
-      }
+  devtool: 'source-map',
+  plugins: [
+    new HTMLWebpackPlugin({ template: `${project.paths.client}/index.html` }),
+    new webpack.DefinePlugin({
+      __DEV__: __DEV__,
+      __PROD__: __PROD__,
+      __TEST__: __TEST__
+    })
+  ],
+  module: { rules: [] }
+}
+
+// Production Configuration
+if (__PROD__) {
+  config.devtool = false
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin('style-[contenthash:10].css')
+  )
+}
+
+// Development Configuration
+if (__DEV__) {
+  config.entry.push(
+    'webpack/hot/dev-server',
+    `webpack-dev-server/client?http://${project.server_host}:${project.server_port}`
+  )
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+  )
+}
+
+// Configuring Loaders
+
+// Only the CSS rules before ExtractTextPlugin
+const cssName = __PROD__ ? '[hash:base64:10]' : '[path][name]---[local]'
+
+config.module.rules.push(
+  {
+    test: /\.scss$/,
+    exclude: /node_modules/,
+    use: [
+      { loader: 'style-loader' },
+      { loader: `css-loader?localIdentName=${cssName}` },
+      { loader: 'sass-loader?sourceMap' }
     ]
   },
-  plugins: plugins
+  {
+    test: /\.css$/,
+    exclude: /node_modules/,
+    use: [
+      { loader: 'style-loader' },
+      { loader: `css-loader?localIdentName=${cssName}` }
+    ]
+  }
+)
+
+if (__PROD__) {
+  debug('Applying ExtractTextPlugin to CSS loaders')
+  config.module.rules = config.module.rules.map(rule => {
+    const loadersToApply = rule.use.slice(1).map(loader => loader.loader)
+    rule.use = ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: loadersToApply
+    })
+    return rule
+  })
 }
+
+config.module.rules.push(
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    use: [{
+      loader: 'babel-loader'
+    }]
+  },
+  {
+    test: /\.(woff|png|jpg|gif)$/,
+    exclude: /node_modules/,
+    use: [{
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: 'images/[hash:12].[ext]'
+      }
+    }]
+  }
+)
 
 module.exports = config
